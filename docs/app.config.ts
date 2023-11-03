f13c0a3550db0a7f35d1d6e8fc4d22265b5fa9de
Markus Geilehner <markus@geilehner.io>
2023-11-03 15:48:45 +0100

    design updates

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -2,9 +2,8 @@ export default defineAppConfig({
   docus: {
     title: 'Nuxt CookieFirst',
     description: 'Nuxt Cookie First is a powerful and easy-to-use Nuxt.js module that seamlessly integrates CookieFirst, a GDPR and CCPA compliant cookie consent solution, into your Nuxt.js application. This module enables developers to manage cookies and user consent with minimal effort while providing an optimal user experience.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    image: 'banner.jpg',
     socials: {
-      twitter: '',
       github: 'https://github.com/markus-gx/nuxt-cookie-first',
       nuxt: {
         label: 'Nuxt',
@@ -29,7 +28,7 @@ export default defineAppConfig({
       fluid: true
     },
     header: {
-      logo: true,
+      logo: false,
       showLinkIcon: true,
       exclude: [],
       fluid: true
